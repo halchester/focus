3b4487cf30c30ddcec97687fda14785ce73877a5
halchester <chester.htooaunglinn@gmail.com>
2021-06-27 20:29:15 +0630

    feat(server): user registering and logging in

--- server/app/controllers/auth.controller.ts
+++ server/app/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import User from '../models/user.model'
 import { Request, Response } from 'express'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req: Request, res: Response) => {
   const { username, password } = req.body
@@ -26,4 +27,38 @@ export const registerUser = async (req: Request, res: Response) => {
   }
 }
 
-export const loginUser = (req: Request, res: Response) => {}
+export const loginUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        error: 'Username or password is wrong!',
+        data: {},
+      })
+    }
+
+    const passwordIsValid = await bcryptjs.compare(password, user.password)
+    if (!passwordIsValid) {
+      return res.status(400).json({ success: false, error: 'Invalid password', data: {} })
+    }
+
+    let token = jwt.sign(
+      { credentials: `${user.uniqueId}.${user.username}` },
+      process.env.JWT_SECRET!,
+      {}
+    )
+
+    const credentials = {
+      uniqueId: user.uniqueId,
+      username: user.username,
+      token,
+    }
+
+    return res.status(200).json({ success: true, data: credentials, error: null })
+  } catch (err) {
+    return res.status(401).json({ success: false, data: 'Username or password is incorrect' })
+  }
+}
